f6cd2fdae04d288bc1bc7bdf45c20a7b2e77b935
dohyeons <ddhhss0603@gmail.com>
2022-08-28 23:55:13 +0900

    숫자야구 시작

---
+++ baseball.js
@@ -0,0 +1,36 @@
+const answerNum = document.querySelector("#answerNumber");
+const answerInput = document.querySelector("#answerInput");
+const inputNum = document.querySelector("#answerInput input");
+const btn = document.querySelector("#answerInput button");
+const pastTry = document.querySelector("#pastTry");
+;
+function answerMaker() {
+    let answer = "";
+    let i = 0;
+    while (i < 3) {
+    answer = answer + Math.floor(Math.random() * 10);
+    i++;
+    }
+    answerNum.innerText = answer;
+}
+function handleSubmit(event) {
+    // input에 적은 숫자를 밑에 쭉 나열
+    // 숫자를 입력받아 문자열로 변환해줌
+    // includes는 true지만 indexOf가 다르다면 ball
+    // includes도 true고 answer.indexOf와 stringNum.indexOf가 같다면 strike
+    // 아니라면 foul
+    event.preventDefault();
+    const stringNum = String(inputNum.value);
+    const past = document.createElement("h2");
+    past.innerText = stringNum;
+    pastTry.appendChild(past);
+    
+}
+
+
+answerInput.addEventListener("submit", handleSubmit);
+
+
+answerMaker();
+
+
