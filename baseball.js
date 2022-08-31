1e921f86da8372425d985b4edec4ae989a2c7696
dohyeons <ddhhss0603@gmail.com>
2022-08-31 23:42:48 +0900

    의사코드 추가

--- baseball.js
+++ baseball.js
@@ -6,7 +6,7 @@ const pastTry = document.querySelector("#pastTry");
 const randomNumBtn = document.querySelector(".container button");
 
 
-function answerMaker() {
+function answerMaker() { // 난수 생성 함수
     let answer = "";
     let i = 0;
     while (i < 3) {
@@ -15,22 +15,27 @@ function answerMaker() {
     }
     answerNum.innerText = answer;
 }
+
+randomNumBtn.addEventListener('click',  answerMaker); // 난수 생성
+
 function handleSubmit(event) {
+    // 정답 입력 버튼 함수
     // input에 적은 숫자를 밑에 쭉 나열
     // 숫자를 입력받아 문자열로 변환해줌
-    // includes는 true지만 indexOf가 다르다면 ball
-    // includes도 true고 answer.indexOf와 stringNum.indexOf가 같다면 strike
-    // 아니라면 foul
     event.preventDefault();
     const stringNum = String(inputNum.value);
     const past = document.createElement("h2");
     past.innerText = stringNum;
     pastTry.appendChild(past);
     inputNum.value='';
+
+    // includes는 true지만 indexOf가 다르다면 ball
+    // includes도 true고 answer.indexOf와 stringNum.indexOf가 같다면 strike
+    // 아니라면 foul
+    
 }
 
 
-answerInput.addEventListener("submit", handleSubmit);
+answerInput.addEventListener("submit", handleSubmit); // 답 입력
 
 
-randomNumBtn.addEventListener('click',  answerMaker)
